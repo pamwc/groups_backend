4ef2b2cefdc94d2a883af598cb2cb9c323944dc6
Dawid <dawid.swinoga@sdqac.rd.tp.pl>
2017-10-31 18:39:26 +0100

    Add swagger 2.

---
+++ src/main/java/edu/groups/server/configuration/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package edu.groups.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Dawid on 31.10.2017 at 18:25.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
