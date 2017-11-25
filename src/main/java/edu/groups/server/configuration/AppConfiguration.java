aae16c7bace443b9e91bd0369e40191edf83d9ce
Dawid <dawid.swinoga@sdqac.rd.tp.pl>
2017-11-25 17:42:15 +0100

    Enable spring async

--- src/main/java/edu/groups/server/configuration/AppConfiguration.java
+++ src/main/java/edu/groups/server/configuration/AppConfiguration.java
@@ -2,12 +2,15 @@ package edu.groups.server.configuration;
 
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Created by Dawid on 01.11.2017 at 14:19.
  */
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+@Configuration
+@EnableAsync
+public class AppConfiguration {
     @Bean
     Gson gson() {
         return new Gson();
